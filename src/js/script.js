c06ad1d0f8b99ff23e6e8ad770442947af532a26
Lorenzo <lorenzocoque@hotmail.com>
2025-04-01 09:11:10 -0300

    DECLARANDO VARIAVEL

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+//DECLARAÇÕES E VARIAVEIS
+var nome="Tralalero Tralala";
+console.log(nome)
+
+let idade =18;
+console.log(idade)
+
+const sobrenome = "Cidade";
+console.log(sobrenome)
+
+let aula;
+console.log(aula)
+
+let valor= null;
+console.log(valor)
+
+let exemplo1={};
+
+let exemplo2=[];
